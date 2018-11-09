0d4c36d6987a4a0e30240c5ebb0dd4534c15acf7
hardgosu <deven1@naver.com>
2018-11-10 02:17:31 +0900

    2DGP term project commit
    차지샷 구현

--- 2DGP project/181022과제용/과제용/Script/갈아엎음/mygame2.py
+++
@@ -1,8 +0,0 @@
-import game_framework2
-import pico2d
-
-import main_state
-
-pico2d.open_canvas(sync = True)
-game_framework2.run(main_state)
-pico2d.close_canvas()
--- 2DGP project/181022과제용/과제용/Script/갈아엎음/objectBase.py
+++
@@ -1,8 +0,0 @@
-import objectManager
-
-def fucker(obj):
-
-    objectManager.allObjects.append(obj)
-
-    print("fucker")
-    pass
